2511955cb6d70a5f6d116657412887b2d48c8d0a
Emily Leones <leonesemily@gmail.com>
2015-09-13 23:49:13 -0700

    Implemented shouldPrintNothingWhenThereAreNoBooks
    in LibraryTests

--- src/test/java/com/thoughtworks/tddintro/library/LibraryTest.java
+++ src/test/java/com/thoughtworks/tddintro/library/LibraryTest.java
@@ -2,6 +2,7 @@ package com.thoughtworks.tddintro.library;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
@@ -9,9 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Matchers.contains;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class LibraryTest {
 
@@ -21,15 +20,20 @@ public class LibraryTest {
         List books tests. Implement the first three tests for the Verify exercise
 
      */
+    private List<String> books;
+    private PrintStream printStream;
 
+    @Before
+    public void setUp() throws Exception {
+        books = new ArrayList<>();
+        printStream = mock(PrintStream.class);
+    }
 
     @Test
     public void shouldPrintBookTitleWhenThereIsOneBook() {
 
-        List<String> books = new ArrayList<>();
         String title = "Book Title";
         books.add(title);
-        PrintStream printStream = mock(PrintStream.class);
         Library library = new Library(books, printStream, null);
 
         library.listBooks();
@@ -41,7 +45,11 @@ public class LibraryTest {
     @Test
     public void shouldPrintNothingWhenThereAreNoBooks() {
 
-        // implement me
+        Library library = new Library(books, printStream, null);
+
+        library.listBooks();
+
+        verifyZeroInteractions(printStream);
     }
 
     @Test
