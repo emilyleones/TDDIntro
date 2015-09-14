9928dd1435dc882bea123dcfc25ac30bcf3b520c
Emily Leones <leonesemily@gmail.com>
2015-09-13 22:08:49 -0700

    Refactored

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,11 +2,7 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i == 0) {
-            return 1;
-        }
-
-        int result = 1;
+       int result = 1;
 
         while (i != 0) {
             result = result * i;
