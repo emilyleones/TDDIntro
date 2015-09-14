aafd2add87e7fd6bf741fac5a53307f9b5d41895
Emily Leones <leonesemily@gmail.com>
2015-09-13 22:11:56 -0700

    Passing shouldThrowIllegalArgumentExceptionWhenNumberIsNegative
    And all preceding test cases.

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,7 +2,12 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-       int result = 1;
+
+        if ( i < 0) {
+            throw new IllegalArgumentException("Argument must be at least zero!");
+        }
+
+        int result = 1;
 
         while (i != 0) {
             result = result * i;
