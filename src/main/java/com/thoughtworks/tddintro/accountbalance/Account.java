5bae16f707084fb0d494c42a8a9cd62df09e90e3
Emily Leones <leonesemily@gmail.com>
2015-09-13 22:38:55 -0700

    Created Test shouldIncreaseMyBalanceWhenIDepositMoney 63

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+public class Account {
+    private int balance;
+
+    public Account(int startBalance) {
+        balance = startBalance;
+    }
+
+    public void deposit(int amount) {
+    }
+
+    public int getBalance() {
+        return -1;
+    }
+}
